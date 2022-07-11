54198f1687faa958062872c770dc2b4999085e91
Eduardo Cachoeira <epcachoeira@hotmail.com>
2022-07-11 12:57:07 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import Logo from '../../assets/img/logo.svg';
+import './styles.css';
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-log-container">
+                <img src={Logo} alt="Logomarca do projeto" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.instagram.com/epcachoeira/">@epcachoeira</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
